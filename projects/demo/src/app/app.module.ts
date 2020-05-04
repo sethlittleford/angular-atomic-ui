629776fd416cbf6f5d66194e70f7f6203b6b52e5
Seth Littleford <seth.littleford@gmail.com>
2020-05-04 14:27:49 -0600

    Imported NavbarModule to the demo app module
    This enabled consumption of the library in the demo app for testing
    and development purposes. The library was built into the dist directory
    per Angular guidelines. Then an npm-link symbolic link was established,
    obviating the need for publishing to the npm registry prematurely. The demo
    app successfully displayed the mock sfl-navbar component.

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NavbarModule } from '@sethlittleford/navbar';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NavbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
