a064b4ae6f0840caa2c495c55708e82a4744cc4a
Seth Littleford <seth.littleford@gmail.com>
2020-05-06 15:37:41 -0600

    Update import statements into demo app module
    library was consolidated and name changed to @sethlittleford/ng-ui.
    The selector prefix was also updated to 'ngs'.

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { NavbarModule } from '@sethlittleford/navbar';
+import { NavbarModule } from '@sethlittleford/ng-ui';
+import { ThemesModule } from '@sethlittleford/ng-ui';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NavbarModule
+    NavbarModule,
+    ThemesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
