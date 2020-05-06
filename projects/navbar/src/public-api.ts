3036cdeb1e4b0a9420ef2f36ce27b15439024e13
Seth Littleford <seth.littleford@gmail.com>
2020-05-06 13:41:33 -0600

    Clean up workspace
    Old navbar library was consolidated into the  new ng-ui library,
    thus directory was deleted and
    angular.json & tsconfig.json updated.

--- projects/navbar/src/public-api.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Public API Surface of navbar.
- * The "user layer" that defines what is available to consumers of this library.
- * Anything exported from this file is made public when this library is imported
- * into an application.
- */
-
-export * from './lib/navbar.component';
-export * from './lib/navbar.module';
