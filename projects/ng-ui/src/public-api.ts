ade21415af91a8bbb2f737a8665f58266fe4b021
Seth Littleford <seth.littleford@gmail.com>
2020-05-06 15:23:41 -0600

    Consolidate themes library into ng-ui
    Old themes library was consolidated into the  new ng-ui library,
    thus directory was deleted and
    angular.json & tsconfig.json updated.

--- projects/ng-ui/src/public-api.ts
+++ projects/ng-ui/src/public-api.ts
@@ -1,12 +1,14 @@
 /*
- * Public API Surface of navbar.
+ * Public API Surface of ng-ui.
  * The "user layer" that defines what is available to consumers of this library.
  * Anything exported from this file is made public when this library is imported
  * into an application.
  */
 
 // navbar
-export * from './navbar/navbar.component';
 export * from './navbar/navbar.module';
+export * from './navbar/navbar.component';
 
 // themes
+export * from './themes/themes.module';
+export * from './themes/themes.component';
--- projects/themes/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of themes
- */
-
-export * from './lib/themes.service';
-export * from './lib/themes.component';
-export * from './lib/themes.module';
