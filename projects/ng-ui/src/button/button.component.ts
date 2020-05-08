94634e6f83e4f001403fab7d18632338aecb472e
Seth Littleford <seth.littleford@gmail.com>
2020-05-08 17:07:00 -0600

    Create button library in ng-ui
    The navbar library can't proceed as an organism until the atomic elements
    such as buttons are developed. The button library represents an atom in
    the overall design.

---
+++ projects/ng-ui/src/button/button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: `button[attribute1]`,
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent implements OnInit {
+  
+  constructor() { }
+  
+  ngOnInit(): void {
+  }
+  
+}
